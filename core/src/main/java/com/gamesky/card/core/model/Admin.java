0ebb0932ff3ac3313bf92cd1631218fddb0af53a
lianghongbin <lianghongbin@autohome.com.cn>
2015-07-26 00:39:38 +0800

    新生成的数据表忘了添加。后台权限完成，数据表也已经添加到线上

---
+++ core/src/main/java/com/gamesky/card/core/model/Admin.java
@@ -0,0 +1,99 @@
+package com.gamesky.card.core.model;
+
+public class Admin {
+    private Integer id;
+
+    private String phone;
+
+    private Boolean top;
+
+    private Boolean locked;
+
+    private Long lastTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getTop() {
+        return top;
+    }
+
+    public void setTop(Boolean top) {
+        this.top = top;
+    }
+
+    public Boolean getLocked() {
+        return locked;
+    }
+
+    public void setLocked(Boolean locked) {
+        this.locked = locked;
+    }
+
+    public Long getLastTime() {
+        return lastTime;
+    }
+
+    public void setLastTime(Long lastTime) {
+        this.lastTime = lastTime;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Admin other = (Admin) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getPhone() == null ? other.getPhone() == null : this.getPhone().equals(other.getPhone()))
+            && (this.getTop() == null ? other.getTop() == null : this.getTop().equals(other.getTop()))
+            && (this.getLocked() == null ? other.getLocked() == null : this.getLocked().equals(other.getLocked()))
+            && (this.getLastTime() == null ? other.getLastTime() == null : this.getLastTime().equals(other.getLastTime()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getPhone() == null) ? 0 : getPhone().hashCode());
+        result = prime * result + ((getTop() == null) ? 0 : getTop().hashCode());
+        result = prime * result + ((getLocked() == null) ? 0 : getLocked().hashCode());
+        result = prime * result + ((getLastTime() == null) ? 0 : getLastTime().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", phone=").append(phone);
+        sb.append(", top=").append(top);
+        sb.append(", locked=").append(locked);
+        sb.append(", lastTime=").append(lastTime);
+        sb.append("]");
+        return sb.toString();
+    }
+}
